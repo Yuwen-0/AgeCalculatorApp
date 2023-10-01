fa4ae843950a6c9348e5a0c9ae79675c9db00e0b
Yuwen-0 <halil.alagul0@gmail.com>
2023-10-01 20:17:34 +0300

    made i responsive

--- src/components/Solution.jsx
+++ src/components/Solution.jsx
@@ -4,9 +4,9 @@ export default function Solution(props){
     return (
         <>
             <div className="solution">
-                <h1><span>{props.years || '--'}</span> years</h1>
+                <h1><span>{props.years >= 0 ? props.years : '--'}</span> years</h1>
                 <h1><span>{props.months >= 0 ? props.months : '--'}</span> months</h1>
-                <h1><span>{props.days || '--'}</span> days</h1>
+                <h1><span>{props.days >= 0 ? props.days : '--'}</span> days</h1>
             </div>
         </>
     )
