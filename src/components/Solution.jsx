04894812d0d557a78271e6011e020d5bf006cff3
Yuwen-0 <halil.alagul0@gmail.com>
2023-10-01 16:38:43 +0300

    solved month not showing when its 0

--- src/components/Solution.jsx
+++ src/components/Solution.jsx
@@ -1,11 +1,19 @@
+import PropTypes from 'prop-types';
+
 export default function Solution(props){
     return (
         <>
             <div className="solution">
                 <h1><span>{props.years || '--'}</span> years</h1>
-                <h1><span>{props.months || '--'}</span> months</h1>
+                <h1><span>{props.months >= 0 ? props.months : '--'}</span> months</h1>
                 <h1><span>{props.days || '--'}</span> days</h1>
             </div>
         </>
     )
+}
+
+Solution.propTypes = {
+    years: PropTypes.number,
+    months: PropTypes.number,
+    days: PropTypes.number,
 }
